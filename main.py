109781bbb45dc952a2e13a5e6956f0da0d9bbc60
Nina Nyberg <96874461+NinaNyberg@users.noreply.github.com>
2023-01-18 14:38:36 +0200

    form handling logic added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from db import *
 
 app = Flask("flask-blogit")
@@ -15,12 +15,18 @@ def home():
     blogs=all_blogs,
     title="HOME")
 
-@app.route("/blogs/create")
+@app.route("/blogs/create", methods=["POST", "GET"])
 def create():
-    return render_template("create.html")
+    if request.method == "GET":
+        return render_template("create.html", title="CREATE BLOG")
+    elif request.method == "POST":
+        save_blog(request.form)
+        return redirect("/")
+
+
 
 @app.route("/about")
 def info():
-    return render_template("about.html")
+    return render_template("about.html", title="ABOUT")
 
 app.run()
